12592abd34335a844dbb447a1310babf4890feda
Gaurav Dwivedi <113124154+gauravdwivediofficial@users.noreply.github.com>
2024-09-11 16:45:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let gameResult = document.getElementById("gameResult");
 let userInput = document.getElementById("userInput");
 let randomNumber = Math.ceil(Math.random() * 100);
+console.log(randomNumber);
 
 function checkGuess() {
     let guessedNumber = parseInt(userInput.value);
